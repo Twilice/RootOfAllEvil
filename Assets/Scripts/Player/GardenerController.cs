218b7a57ec250d0c0f1b681a58516b2c4670d0c7
Henrik Risberg <48048662+RisaJr@users.noreply.github.com>
2023-02-04 13:44:21 +0100

    Add the Gardener

---
+++ Assets/Scripts/Player/GardenerController.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenerController : MonoBehaviour
+{
+    // Publics
+    public Transform m_body;
+    public float m_moveSpeed = 10f;
+    public float m_swingSpeed = 0.25f;
+    public float m_rotationAngle = 90f;
+    public Transform m_axePivot;
+    
+    // Privates
+    private Quaternion originalRotation;
+    private bool rotateBack = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_axePivot.localEulerAngles = new Vector3(0f, -(m_rotationAngle / 2f), 0f);
+        originalRotation = m_axePivot.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(horizontal, 0, vertical) * m_moveSpeed * Time.deltaTime;
+
+        //Rotation
+        Vector3 mousePosition = Input.mousePosition;
+        Vector3 viewportPoint = Camera.main.ScreenToViewportPoint(mousePosition);
+        Vector3 target = Camera.main.ViewportToWorldPoint(new Vector3(viewportPoint.x, viewportPoint.y, m_body.position.z - Camera.main.transform.position.z));
+        m_body.LookAt(target, Vector3.up);
+        //Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(m_body.position);
+        //Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen) ;
+
+        //m_body.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+
+
+        // Swing
+        if (m_axePivot.localRotation == originalRotation && Input.GetKeyDown(KeyCode.Space))
+        {
+            rotateBack = false;
+            StartCoroutine(SmoothRotate(m_rotationAngle));
+        }
+    }
+
+    IEnumerator SmoothRotate(float angle)
+    {
+        float startTime = Time.time;
+        float endTime = startTime + m_swingSpeed;
+        float progress = 0;
+        Quaternion startRotation = m_axePivot.localRotation;
+        Quaternion endRotation = m_axePivot.localRotation * Quaternion.Euler(0, angle, 0);
+
+        while (progress < 1)
+        {
+            progress = (Time.time - startTime) / (endTime - startTime);
+            m_axePivot.localRotation = Quaternion.Slerp(startRotation, endRotation, progress);
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (!rotateBack)
+        {
+            rotateBack = true;
+            StartCoroutine(SmoothRotate(-angle));
+        }
+        else
+        {
+            m_axePivot.localRotation = originalRotation;
+        }
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
