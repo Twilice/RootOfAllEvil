4412579e96baa7cd56ceae0bdb7ecae7c796e380
Henrik Risberg <48048662+RisaJr@users.noreply.github.com>
2023-02-05 12:43:48 +0100

    Seperated Axe and Body Sounds on Gardener

--- Assets/Scripts/Player/GardenerController.cs
+++ Assets/Scripts/Player/GardenerController.cs
@@ -30,9 +30,11 @@ public class GardenerController : MonoBehaviour
     public ParticleSystem m_levelUpEffect;
 
     [Header("Sounds")]
-    public AudioSource m_audioSource;
+    public AudioSource m_axeAudioSource;
     public AudioClip m_swooshSoundClip;
+    public AudioSource m_bodyAudioScorce;
     public AudioClip m_gettingHit;
+    public AudioClip m_levelUpClip;
 
     // Privates
     private Quaternion originalRotation;
@@ -68,6 +70,8 @@ public class GardenerController : MonoBehaviour
             _lvl = value;
             if (_lvl > oldLVL)
             {
+                m_bodyAudioScorce.clip = m_levelUpClip;
+                m_bodyAudioScorce.Play();
                 m_levelUpEffect.Play();
             }
         }
@@ -88,8 +92,8 @@ public class GardenerController : MonoBehaviour
 
             if (_hp < oldHP)
             {
-                m_audioSource.clip = m_gettingHit;
-                PlaySoundClip();
+                m_bodyAudioScorce.clip = m_gettingHit;
+                m_bodyAudioScorce.Play();
                 StartCoroutine(ScreenShake(0.2f, 0.3f));
             }
         }
@@ -133,7 +137,7 @@ public class GardenerController : MonoBehaviour
         if (workaroundAttackCooldown && Input.GetKeyDown(KeyCode.Mouse0))
         {
             rotateBack = false;
-            m_audioSource.clip = m_swooshSoundClip;
+            m_axeAudioSource.clip = m_swooshSoundClip;
             //StartCoroutine(SmoothRotate(m_rotationAngle));
             swingAnimator.SetTrigger("Swing");
             CheckForRoots(out bool missedRoot);
@@ -252,7 +256,7 @@ public class GardenerController : MonoBehaviour
             frameLastPitchChange = Time.frameCount;
             pitch = UnityEngine.Random.Range(0.96f, 1.05f);
         }
-        m_audioSource.pitch = pitch;
-        m_audioSource.Play();
+        m_axeAudioSource.pitch = pitch;
+        m_axeAudioSource.Play();
     }
 }
