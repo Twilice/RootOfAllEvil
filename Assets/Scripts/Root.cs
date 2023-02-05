4989b5881c239ba68eeb039f1f76128f39c11a42
Valgarður Ragnheiðar Ívarsson <24223330+ValgardurRI@users.noreply.github.com>
2023-02-05 13:27:02 +0100

    Fixed camera shake

--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -230,6 +230,8 @@ public class Root : MonoBehaviour
         {
             flowerInstance.Die();
         }
+        
+        Camera.main.GetComponent<CameraFollow>().ShakeCamera(0.1f, 0.03f);
         Destroy(this.gameObject);
     }
     private static float pitch = 1;
